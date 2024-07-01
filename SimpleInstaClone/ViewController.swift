a5191eab23e5f26c8472ad2b3156b333c7c4dc76
mrtoktm <mrtoktm9@gmail.com>
2024-07-01 13:58:50 +0300

    Initial Commit

---
+++ SimpleInstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleInstaClone
+//
+//  Created by Mert Öktem on 1.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
