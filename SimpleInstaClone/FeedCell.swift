52b42c4544a6a25e81353a4c17f496b3c0685b2a
mrtoktm <mrtoktm9@gmail.com>
2024-07-02 15:34:17 +0300

    last updated; add user sign in, sign up, like button, upload and share photos using firebase

---
+++ SimpleInstaClone/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  SimpleInstaClone
+//
+//  Created by Mert Öktem on 1.07.2024.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
